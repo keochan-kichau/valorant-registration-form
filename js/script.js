fe8cb8b66f5ecc9218ed848deb1584455aaf8b38
keochan-kichau <filmdanh@gmail.com>
2025-10-07 12:12:33 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,11 +9,14 @@ document
       obj[key] = value;
     });
     // ví dụ: gửi tới endpoint API bạn viết hoặc service form:
-    fetch("https://example.com/api/register", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(obj),
-    })
+    fetch(
+      "https://script.google.com/macros/s/AKfycbztdjAjqMe_Qeit4mA_nvGNJdzpQJ-rIaiZdwdc-enDI3wo0-I8uNOANTtwyQJ-G5wCCA/exec",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(obj),
+      }
+    )
       .then((res) => {
         if (res.ok) alert("Đăng ký thành công!");
         else alert("Có lỗi, thử lại sau.");
